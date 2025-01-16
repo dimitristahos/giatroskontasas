7bd20756acc9a07fcbf4efe3f565579f97c8e972
dtahos <dimitristahos@gmal.com>
2025-01-16 22:25:56 +0200

    Added nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/content"],
+  modules: ["@nuxt/content", "@nuxt/ui"],
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   content: {},
