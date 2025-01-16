5fd4ea9b56668c25363e2a1479273f0bcc7e72dc
dtahos <dimitristahos@gmal.com>
2025-01-16 22:12:06 +0200

    Test 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/content", "@nuxthq/studio"],
+  modules: ["@nuxt/content"],
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  content: {
-    preview: {
-      api: "https://api.nuxt.studio",
-    },
-  },
 });
