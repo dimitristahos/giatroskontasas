db6a34cbab64d0c1e64e588981cf491255abff69
dtahos <dimitristahos@gmal.com>
2025-01-18 10:10:39 +0200

    Test: Added nuxt schema

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,7 @@
-import { field, group } from "@nuxt/content/preview";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxt/content", "@nuxt/ui"],
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   content: {},
-  appConfig: {
-    parent: group({
-      title: "UI",
-      description: "UI configuration",
-      icon: "i-ph-palette-fill",
-      fields: {
-        primary: field({
-          type: "string",
-          title: "Primary",
-          description: "Primary color of your UI.",
-          icon: "i-ph-palette",
-          default: "sky",
-          required: ["sky", "mint", "rose", "amber"],
-        }),
-      },
-    }),
-  },
 });
