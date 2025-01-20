9b71f82875eb0a801a06722429385c9c5c6d6bfc
Leonidas Michalopoulos <mich.lewn@gmail.com>
2025-01-20 16:21:28 +0200

    add: images. refactor: hero section

--- components/index/Hero.vue
+++ components/index/Hero.vue
@@ -1,28 +1,17 @@
 <template>
-    <div class="bg-[#E1EBE6] min-h-[60vh] lg:min-h-[70vh] flex items-center px-4 lg:px-52">
+    <div class="relative bg-[url('/public/images/dr-hero-bg.png')] bg-cover bg-center bg-black/50 bg-blend-multiply min-h-[60vh] lg:min-h-[80vh] flex items-center px-4 lg:px-52">
         <div class="flex flex-col lg:flex-row justify-between lg:items-center w-full">
             <!-- Left Section -->
             <div class="lg:w-1/2 lg:text-left">
-                <h1 class="text-3xl lg:text-6xl font-semibold text-gray-900 leading-snug mb-5 lg:mb-0">
+                <h1 class="text-3xl lg:text-6xl font-semibold text-white !leading-tight mb-5 lg:mb-0">
                     Ο προσωπικός σας <br /> γιατρός, πάντα κοντά σας.
                 </h1>
-            </div>
-            <!-- Right Section -->
-            <div class="lg:w-1/2 flex flex-col items-center">
-                <div>
-                    <p class="text-xl lg:text-2xl text-gray-700 mb-8 lg:leading-relaxed max-w-[550px]">
+                <p class="text-xl text-gray-200 mb-8 lg:leading-relaxed max-w-[550px] mt-6">
                         Η υγεία σας είναι προτεραιότητά μας. Βρείτε Γιατρούς που νοιάζονται, κλείστε ραντεβού εύκολα και
                         λάβετε τη φροντίδα που αξίζετε.
-                    </p>
-                    <div class="flex gap-4">
-                        <button class="bg-gray-900 text-white py-2 px-6 rounded-3xl hover:bg-gray-800">
-                            Ενδιαφέρομαι
-                        </button>
-                        <button
-                            class="bg-transparent text-gray-900 border border-gray-900 py-2 px-4 rounded-3xl hover:bg-gray-100">
-                            Μάθε περισσότερα
-                        </button>
-                    </div>
+                </p>
+                <div class="flex gap-4">
+                    <a class="cursor-pointer group relative inline-flex h-10 px-5 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-white font-medium"><div class="inline-flex h-12 translate-y-0 items-center justify-center px-6 text-neutral-950 transition duration-500 group-hover:-translate-y-[150%]">Ενδιαφέρομαι</div><div class="absolute inline-flex h-12 w-full translate-y-[100%] items-center justify-center text-neutral-950 transition duration-500 group-hover:translate-y-0"><span class="text-neutral-950 absolute h-full w-full translate-y-full skew-y-12 scale-y-0 transition duration-500 group-hover:translate-y-0 group-hover:scale-150"></span><span class="z-10">Ενδιαφέρομαι</span></div></a>
                 </div>
             </div>
         </div>
