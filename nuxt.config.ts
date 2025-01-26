e8d34eab4f40d1a609bfacf24feec86236df8642
dtahos <dimitristahos@gmal.com>
2025-01-26 20:33:48 +0200

    Test: Disable SSR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxt/content", "@nuxt/ui", "@nuxtjs/turnstile", "@nuxt/icon"],
+  ssr: false,
   runtimeConfig: {
     public: {
       baseUrl: process.env.BASE_URL || "http://localhost:3000",
