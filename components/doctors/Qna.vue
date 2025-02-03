c9efd03f58a9e184fd31c2db1b10cc255c0bb2d1
Leonidas Michalopoulos <mich.lewn@gmail.com>
2025-02-03 17:17:46 +0200

    feat: add sections giatros page chore: add content giatros page

---
+++ components/doctors/Qna.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const items = [{
+  label: 'Πώς μπορώ να γίνω συνεργάτης;',
+  defaultOpen: true,
+  content: 'Συμπληρώστε τη φόρμα εγγραφής και θα σας ενημερώσουμε για τα επόμενα βήματα.'
+}, {
+  label: 'Πόσα χρήματα μπορώ να κερδίσω;',
+  content: ' Το εισόδημά σας εξαρτάται από τις υπηρεσίες που παρέχετε και τον αριθμό των ραντεβού ή διανομών που ολοκληρώνετε.'
+}, {
+  label: ' Ποιος μπορεί να γίνει συνεργάτης;',
+  content: 'Γιατροί. και διανομείς που θέλουν να συμβάλουν στην υγεία και την εξυπηρέτηση των ασθενών.'
+}, {
+  label: 'Πώς λαμβάνω πληρωμές;',
+  content: 'Οι πληρωμές γίνονται ηλεκτρονικά ανά ασθενή.'
+},]
+</script>
+
+<template>
+    <section class="px-2 lg:px-52 mb-3 py-16 mx-auto lg:text-center max-w-6xl">
+      <h2 class="text-4xl font-bold mb-8">Συχνές ερωτήσεις</h2>
+      <div class="text-start">
+          <UAccordion
+              :items="items"
+              :ui="{
+                  header: {
+                      base: 'text-xl font-semibold text-neutral-950',
+                  }
+              }"
+            />
+      </div>
+    </section>
+    <div class="border-b border-neutral-300"></div>
+</template>
--- pages/giatros.vue
+++ pages/giatros.vue
@@ -1,41 +1,67 @@
 <template>
     <section class="px-4 lg:px-52 py-16">
-        <div class="flex flex-wrap justify-between items-center mb-12">
-            <h1 class="text-4xl lg:text-5xl mb-5 lg:mb-0 font-semibold max-w-[650px] lg:leading-snug">Ο προσωπικός σας γιατρός, πάντα κοντά σας.</h1>
+        <div class="flex flex-wrap justify-between mb-12">
+            <h1 class="text-4xl lg:text-5xl mb-5 lg:mb-0 font-semibold max-w-[650px] lg:leading-snug">Γίνε συνεργάτης με το "Γιατρός Κοντά Σας"!</h1>
             <div>
-                <p class="mb-5 max-w-[620px] text-lg opacity-80">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci obcaecati sapiente culpa cupiditate consectetur consequatur nihil impedit quo. Magnam voluptatum et dolorum tempore? Quia, perferendis?</p>
+                <p class="max-w-[620px] text-xl font-bold">2 Τρόποι Συνεργασίας:</p>
+                <ul class="mb-4 max-w-[620px]">
+                    <li class="text-lg mb-3"><span class="font-semibold">Γιατρός</span>: Παρέχετε υπηρεσίες κατ' οικον και κλείνετε ραντεβού μέσω της πλατφόρμας μας.</li>
+                    <li class="text-lg"><span class="font-semibold">Διανομέας</span>: Μεταφέρετε φάρμακα ή ιατρικό εξοπλισμό στους ασθενείς.</li>
+                </ul>
+                <div class="border-t mb-2"></div>
+                <p class="mb-5 max-w-[620px] opacity-80">Ξεκίνα εύκολα και κέρδισε χρήματα βοηθώντας ανθρώπους να βρουν τη φροντίδα που χρειάζονται.</p>
                 <a class="cursor-pointer group relative inline-flex h-10 px-5 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-white font-medium"><div class="inline-flex h-12 translate-y-0 items-center justify-center px-6 text-neutral-950 transition duration-500 group-hover:-translate-y-[150%]">Ενδιαφέρομαι</div><div class="absolute inline-flex h-12 w-full translate-y-[100%] items-center justify-center text-neutral-950 transition duration-500 group-hover:translate-y-0"><span class="text-neutral-950 absolute h-full w-full translate-y-full skew-y-12 scale-y-0 transition duration-500 group-hover:translate-y-0 group-hover:scale-150"></span><span class="z-10">Ενδιαφέρομαι</span></div></a>
             </div>
         </div>
         <img src="/public/images/home-visit.jpg" alt="" class="rounded-xl">
         <div class="mt-16">
             <h2 class="text-3xl font-semibold mb-5">Πως δουλεύει;</h2>
-            <p class="max-w-[820px] text-xl opacity-80 leading-relaxed">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia reprehenderit natus laudantium eveniet ab autem deleniti. Natus odio eaque dicta iste molestiae tempore provident ex dolore aut, amet commodi ut, deserunt illo. Distinctio perferendis quidem at totam autem sequi perspiciatis exercitationem est nam, eius suscipit sit vitae nisi? Sint.</p>
+            <p class="max-w-[820px] text-xl opacity-80 leading-relaxed mb-8">Ξεκινήστε σε 3 απλά βήματα: Κάντε Αίτηση συμπληρώνοντας τη φόρμα συνεργασίας. Περιμένετε την Έγκριση, Η ομάδα μας θα αξιολογήσει την αίτησή σας. Ξεκινήστε & Κερδίστε παρέχοντας τις υπηρεσίες σας και κερδίστε χρήματα!</p>
+            <h3 class="text-lg font-semibold mb-3">Μερικά πράγματα που θα χρειαστείτε για να ξεκινήσετε:</h3>
+            <ul class="list-disc pl-5">
+                <li>Άδεια Ασκήσεως Επαγγέλματος και έτος ειδικότητας.</li>
+                <li>Πρόσβαση στο διαδίκτυο και κινητό τηλέφωνο.</li>
+                <li>Διάθεση για συνεργασία και εξυπηρέτηση.</li>
+            </ul>
             <div class="flex justify-center lg:justify-between gap-4 lg:gap-0 items-center flex-wrap mt-12">
                 <!-- STEP 1 -->
                 <div class="rounded-lg shadow border border-gray-300 max-w-[450px]">
                     <img src="/public/images/mockup-1.webp" alt="" class="mb-3 rounded-t-lg">
                     <div class="p-4">
-                        <h4 class="text-xl font-semibold">Βήμα 1</h4>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto dolores dicta eligendi officia tenetur reprehenderit tempore at minima exercitationem eaque.</p>
+                        <h4 class="text-xl font-semibold mb-3">Καταγραφή Αναγκών</h4>
+                        <p class="opacity-80 text-md">Ο πολίτης συμπληρώνει μια εύχρηστη ηλεκτρονική φόρμα ή επικοινωνεί τηλεφωνικά μαζί μας, περιγράφοντας: Το πρόβλημα υγείας ή την υπηρεσία που χρειάζεται.Την τοποθεσία και την επιθυμητή ημέρα/ώρα εξυπηρέτησης. Οποιαδήποτε ειδική πληροφορία σχετίζεται με την κατάστασή του. Με αυτήν την αναλυτική περιγραφή, εξασφαλίζουμε ότι θα συνδεθεί με τον κατάλληλο επαγγελματία υγείας.</p>
                     </div>
                 </div>
                 <!-- STEOP 2 -->
                 <div class="rounded-lg shadow border border-gray-300 max-w-[450px]">
                     <img src="/public/images/mockup-2.webp" alt="" class="mb-3 rounded-t-lg">
                     <div class="p-4">
-                        <h4 class="text-xl font-semibold">Βήμα 2</h4>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto dolores dicta eligendi officia tenetur reprehenderit tempore at minima exercitationem eaque.</p>
+                        <h4 class="text-xl font-semibold mb-3">Άμεση Ανάθεση σε Υπεύθυνο</h4>
+                        <p class="opacity-80 text-md">
+                            Μόλις λάβουμε την αίτησή σας η ομάδα μας επεξεργάζεται τις πληροφορίες και εντοπίζει τον κατάλληλο ιατρό ή επαγγελματία υγείας, ανάλογα με την ειδικότητα που απαιτείται. Ο υπεύθυνος ιατρός λαμβάνει όλες τις απαραίτητες λεπτομέρειες και προγραμματίζει την επίσκεψη. Εσείς λαμβάνετε άμεση ενημέρωση για την επίσκεψη και τις λεπτομέρειες της συνάντησης.
+                        </p>
                     </div>
                 </div>
                 <!-- STEP 3 -->
                 <div class="rounded-lg shadow border border-gray-300 max-w-[450px]">
                     <img src="/public/images/mockup-3.webp" alt="" class="mb-3 rounded-t-lg">
                     <div class="p-4">
-                        <h4 class="text-xl font-semibold">Βήμα 3</h4>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto dolores dicta eligendi officia tenetur reprehenderit tempore at minima exercitationem eaque.</p>
+                        <h4 class="text-xl font-semibold mb-3">Παροχή Υπηρεσίας στο Σπίτι σας</h4>
+                        <p class="opacity-80 text-md">
+                            Ο ιατρός φτάνει στην τοποθεσία που έχετε δηλώσει, εξοπλισμένος με όλα τα απαραίτητα. Εκτελεί τη θεραπεία, εξέταση ή διαδικασία που έχετε ζητήσει. Παρέχει συμβουλές για την περαιτέρω διαχείριση της υγείας σας. Εάν χρειαστεί, οργανώνεται παρακολούθηση ή επόμενες επισκέψεις.
+                        </p>
                     </div>
                 </div>
+            </div>
+            <!-- Why US -->
+            <h2 class="text-3xl font-semibold mb-5 mt-12">Γιατί να μας Εμπιστευτείτε;</h2>
+            <ul class="list-disc pl-5 text-lg max-w-[650px]">
+                <li class="mb-3">Άμεση ανταπόκριση: Εξυπηρέτηση σύμφωνα με την ημέρα και ώρα που εξυπηρετεί καλύτερα εσάς.</li>
+                <li class="mb-3">Εξειδικευμένο προσωπικό: Μόνο έμπειροι και πιστοποιημένοι επαγγελματίες υγείας.</li>
+                <li class="mb-3">Εξατομίκευση: Προσαρμοσμένες υπηρεσίες με βάση τις ανάγκες σας.</li>
+            </ul>
+            <div class="mt-5">
+                <DoctorsQna />
             </div>
         </div>
     </section>
